8eabc141806b641e8b58d7eaafb685fe2fcf6798
haeing0v0 <sm134884@gmail.com>
2026-04-30 18:11:56 +0900

    e-약은요 검색 api 연동 확인 완료

--- medientApi/src/main/java/org/medient/controller/DrugController.java
+++ medientApi/src/main/java/org/medient/controller/DrugController.java
@@ -1,5 +1,21 @@
 package org.medient.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.medient.service.DrugService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/drugs")
+@RequiredArgsConstructor
 public class DrugController {
 
+	private final DrugService drugService;
+
+	@GetMapping("/search")
+	public String searchDrug(@RequestParam("keyword") String keyword) {
+		return drugService.searchDrug(keyword);
+	}
 }
--- medientApi/src/main/java/org/medient/service/DrugService.java
+++ medientApi/src/main/java/org/medient/service/DrugService.java
@@ -1,5 +1,43 @@
 package org.medient.service;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
 public class DrugService {
 
+	private final RestClient restClient = RestClient.create();
+
+	@Value("${openapi.service-key}")
+	private String serviceKey;
+
+	@Value("${openapi.easy-drug-url}")
+	private String easyDrugUrl;
+
+	public String searchDrug(String keyword) {
+
+		String encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8);
+
+		String fullUrl = UriComponentsBuilder.fromUriString(easyDrugUrl)
+				.queryParam("serviceKey", serviceKey)
+				.queryParam("pageNo", 1)
+				.queryParam("numOfRows", 10)
+				.queryParam("type", "json")
+				.queryParam("itemName", encodedKeyword)
+				.build(true)
+				.toUriString();
+
+		System.out.println("URL: " + fullUrl);
+
+		return restClient.get()
+				.uri(URI.create(fullUrl))
+				.retrieve()
+				.body(String.class);
+	}
 }
