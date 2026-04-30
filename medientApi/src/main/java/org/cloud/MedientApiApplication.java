2136b5664d681b3ec9c5a059a3fad553b4bd307c
haeing0v0 <sm134884@gmail.com>
2026-04-30 14:30:31 +0900

    first commit

---
+++ medientApi/src/main/java/org/cloud/MedientApiApplication.java
@@ -0,0 +1,13 @@
+package org.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedientApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedientApiApplication.class, args);
+	}
+
+}
